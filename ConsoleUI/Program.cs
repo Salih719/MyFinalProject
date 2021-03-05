d12b96691bad4b94f15668211ad08ee586671b7a
Salih Kurt <matrixmehmett@hotmail.com>
2021-03-05 07:26:46 +0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //SOLID
+        //Open Closed Principle
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var i in categoryManager.GetAll())
+            {
+                Console.WriteLine(i.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+        }
+    }
+}
